16bf8193afed40f74cf2d6d5a801b72c426b3654
Aaron <apbruno@ucsc.edu>
2023-05-05 17:37:00 -0700

    Changes to game.js, updated Readme

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 Creating an opening video game cinematic using phaser
 
 Key requirements for this assignment
-- **3+ scenes**: A logo, title and main menu scene
+- **3+ scenes**: A "click-to-start" scene, logo, title and main menu scene
 - **2+ audio assets**: rubber ducky sound effect, swoosh sound effect, wild west song (Credits listed below)
-- **2+ image assets**: ...
+- **2+ image assets**: 2 rubber duckys on logo scene, background art on menu menu scene
 - **2+ geometric shapes**: 2 rectangles used as part of the logo scene
 - **1+ logotype**: Rubber Ducky Entertainment made in Adobe Photoshop
-- **1+ multi-line text**: Menu text
-- **1+ fading scene transition**: Fade out transition from logo scene to title scene
+- **1+ multi-line text**: Menu text ("Play", "Load Game", "Options", "Quit")
+- **1+ fading scene transition**: Fade out transition from logo scene to title scene, fade out transition from title scene to main menu scene, fade in transition into main menu scene
 - **1+ object motion animation**: All transitions created using tweens
 
 Sound/Music Credits:
--- game.js
+++ game.js
@@ -4,6 +4,9 @@ class Intro extends Phaser.Scene {
     }
 
     create() {
+        this.cameras.main.setBackgroundColor('0xB19CD8');
+
+
         let clickText = this.add.text(10,10,"Click to start.");
 
         clickText.setX(this.game.config.width / 2 - clickText.width / 2);
@@ -23,10 +26,12 @@ class LogoScene extends Phaser.Scene {
         this.load.image('ducky', '/assets/PixelDucky.png');
         
         this.load.audio('squeaky', '/assets/rubberduck_edited.mp3');
-        console.log('audio loaded');
+        console.log('Images and audio loaded, LogoScene');
     }
 
     create() {
+        this.cameras.main.setBackgroundColor('0xB19CD8');
+        
         // Add logo image, and scale the logo
         let logo = this.add.image(400, 300, 'logo').setScale(0);
         logo.alpha = 1;
@@ -83,9 +88,19 @@ class LogoScene extends Phaser.Scene {
         });
         rBotTween.play();
 
+        // Rubberducky squeak sound effect
         let squeak = this.sound.add('squeaky');
-        squeak.play({ delay: 1000 });
+        squeak.play({ delay: 1.7 });
 
+        // Camera fade out after a delay of 2000 ms, fade-out lasting 1000 ms
+        this.time.delayedCall(3500, function () {
+            this.cameras.main.fadeOut(2500);
+        }, [], this);
+        
+        // Transition to title scene
+        this.time.delayedCall(6000, () => {
+            this.scene.start('title');
+        });
     }
 }
 
@@ -95,11 +110,39 @@ class Title extends Phaser.Scene {
     }
 
     preload() {
-
+        this.load.audio('swoosh', '/assets/Swoosh_Edited.mp3');
+        console.log('Audio loaded, TitleScene');
     }
 
     create() {
+        let wind = this.sound.add('swoosh');
+        wind.play({ volume: 3, delay: 0.35 });
 
+        // create a new text object
+        let titleText = this.add.text(-game.config.width, 250, 'The Last Gunslinger', {
+            fontFamily: 'Brush Script MT',
+            fontSize: 90,
+            color: '#dd571c'
+        });
+
+        // create a tween to move the text from left to right
+        this.tweens.add({
+            targets: titleText,
+            x: game.config.width / 2 - titleText.width / 2,
+            ease: 'Power1',
+            duration: 1000,
+            delay: 1000
+        });
+
+        // Camera fade out after a delay of 4000 ms, fade-out lasting 2500 ms
+        this.time.delayedCall(4000, function () {
+            this.cameras.main.fadeOut(2500);
+        }, [], this);
+        
+        // Transition to Menu scene
+        this.time.delayedCall(7000, () => {
+            this.scene.start('menu');
+        });
     }
 }
 
@@ -109,10 +152,97 @@ class Menu extends Phaser.Scene {
     }
 
     preload() {
-
+        this.load.image('mainScreen', '/assets/MainScreen.png');
+        
+        this.load.audio('wildwest', '/assets/WildWest_Edited.mp3');
+        console.log('Images and audio loaded, MenuScene');
     }
 
     create() {
+        // Camera fade out after a delay of 4000 ms, fade-out lasting 2500 ms
+        this.time.delayedCall(0, function () {
+            this.cameras.main.fadeIn(4000);
+        }, [], this);
+
+        let image = this.add.sprite(game.scale.width / 2 , game.scale.height / 2,'mainScreen');
+
+        // Set the image scale based on the calculated scaleFactor
+        image.setScale(0.65, 0.85);
+
+        let song = this.sound.add('wildwest');
+        song.play({ volume: 0.4, delay: 0.35, loop: true });
+
+
+        // create a new text object for "Play"
+        let playText = this.add.text(-game.config.width, 385, 'Play ', {
+            fontFamily: 'Impact',
+            fontSize: 80,
+            color: '#000000'
+        });
+        // create a tween to move the text from left to right
+        this.tweens.add({
+            targets: playText,
+            x: 645,
+            ease: 'Power1',
+            duration: 1500,
+            delay: 1000
+        });
+        // Set the shadow properties
+        playText.setShadow(3, 3, 'rgba(0, 0, 0, 0.5)', 3);
+
+
+        // create a new text object for "Load Game"
+        let loadText = this.add.text(-game.config.width, 475, 'Load Game ', {
+            fontFamily: 'Impact',
+            fontSize: 24,
+            color: '#000000'
+        });
+        // create a tween to move the text from left to right
+        this.tweens.add({
+            targets: loadText,
+            x: 670,
+            ease: 'Power1',
+            duration: 1500,
+            delay: 1300
+        });
+        // Set the shadow properties
+        loadText.setShadow(3, 3, 'rgba(0, 0, 0, 0.5)', 3);
+
+
+        // create a new text object for "Options"
+        let optionsText = this.add.text(-game.config.width, 510, 'Options ', {
+            fontFamily: 'Impact',
+            fontSize: 24,
+            color: '#000000'
+        });
+        // create a tween to move the text from left to right
+        this.tweens.add({
+            targets: optionsText,
+            x: 700,
+            ease: 'Power1',
+            duration: 1500,
+            delay: 1600
+        });
+        // Set the shadow properties
+        optionsText.setShadow(3, 3, 'rgba(0, 0, 0, 0.5)', 3);
+
+
+        // create a new text object for "Quit"
+        let quitText = this.add.text(-game.config.width, 540, 'Quit ', {
+            fontFamily: 'Impact',
+            fontSize: 36,
+            color: '#000000'
+        });
+        // create a tween to move the text from left to right
+        this.tweens.add({
+            targets: quitText,
+            x: 715,
+            ease: 'Power1',
+            duration: 1500,
+            delay: 1900
+        });
+        // Set the shadow properties
+        quitText.setShadow(3, 3, 'rgba(0, 0, 0, 0.5)', 3);
 
     }
 }
@@ -121,7 +251,7 @@ let config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    backgroundColor: 0xB19CD8,
+    backgroundColor: 0x000000,
     scene: [Intro, LogoScene, Title, Menu]
 }
 
